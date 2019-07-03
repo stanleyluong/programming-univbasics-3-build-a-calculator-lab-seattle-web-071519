1d6e758ea55f92a615bdbe809e99388a035edb66
crisp-convention-4859 <slluong@uw.edu>
2019-07-03 15:51:33 +0000

    Done.

--- lib/math.rb
+++ lib/math.rb
@@ -1,23 +1,29 @@
 def addition(num1, num2)
-
+  addition = num1 + num2
+  p addition
 end
 
 def subtraction(num1, num2)
-
+  subtraction = num1 - num2
+  p subtraction
 end
 
 def division(num1, num2)
-
+  division = num1 / num2
+  p division
 end
 
 def multiplication(num1, num2)
-
+  multiplication = num1 * num2
+  p multiplication
 end
 
 def modulo(num1, num2)
-
+  modulo = num1 % num2
+  p modulo
 end
 
 def square_root(num)
-
+  square_root = Math.sqrt(num)
+  p square_root
 end
